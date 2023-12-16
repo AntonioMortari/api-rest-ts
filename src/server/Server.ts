896c1f4c2c075e873f9f71614bc297012b02b7ce
Antonio Mortari <antoniomortari09@gmail.com>
2023-12-16 10:29:12 -0300

    initial config

---
+++ src/server/Server.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from 'express'
+
+const server: Application = express()
+
+server.get('/', (req: Request, res: Response) => {
+    res.status(200).json({ message: 'Ok' })
+})
+
+export { server }
